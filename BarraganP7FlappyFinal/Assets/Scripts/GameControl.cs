067e8927cfc5f5738a9514285143abcc1261af35
728843-ui <728843@student.dusd.net>
2025-12-12 21:51:30 -0800

    finished flappy bird

---
+++ BarraganP7FlappyFinal/Assets/Scripts/GameControl.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+
+    public static GameControl Instance;
+    public GameObject gameOverText;
+    public Text scoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Awake ()
+    {
+        if (Instance == null) 
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy (gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (gameOver == true && Input.GetMouseButtonDown (0))
+       {
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+       }
+    }
+    
+    public void birdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score " + score.ToString ();
+    }
+   
+        public void birdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+    
+}
